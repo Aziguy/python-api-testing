15da1ab36e8aea4564d6771ccfc4467a356d095b
Aziguy <monsieur_hernandez@hotmail.com>
2024-08-08 13:14:17 +0200

    Optimize our test_basic using best coding practices.

--- APIPestStoreTests/test_basics.py
+++ APIPestStoreTests/test_basics.py
@@ -1,40 +1,48 @@
 import allure
-import json
 import requests
-
-base_url = 'https://petstore.swagger.io/v2/pet/'
-petID = '150'
-
-
-# test valid response or response is not empty
-def test_get_pet_by_id_response():
-    url = base_url + petID
-    header = {'Content-Type': 'application/json'}
-    print("RequestURL: ", url)
-    response = requests.get(url, verify=False, headers=header)
-    data = response.json()
-    print(json.dumps(data, indent=3))
-    assert len(data) > 0, "empty response"
+from typing import Dict, Tuple
+from dataclasses import dataclass
+from urllib.parse import urljoin
 
 
-# testing response body for "ID" key
-@allure.step('Doing get pet by ID Tests')
-def test_get_pet_by_id_id():
-    url = base_url + petID
-    header = {'Content-Type': 'application/json'}
-    print("RequestURL: ", url)
-    response = requests.get(url, verify=False, headers=header)
-    data = response.json()
-    assert data['id'] == int(petID)
+@dataclass
+class Config:
+    base_url: str = 'https://petstore.swagger.io/v2/pet/'
+    pet_id: int = 150
+    new_pet_id: int = 191
+    new_pet_name: str = 'Cutie'
+    new_pet_status: str = 'available'
 
 
-# test adding new pet to store
-def test_add_new_pet():
-    url = base_url
-    header = {'Content-Type': 'application/json'}
-    payload = {"id": 191, "name": "Cutie", "status": "available"}
-    response = requests.post(url, verify=False, json=payload, headers=header)
-    data = response.json()
-    assert data['id'] == 191
-    assert len(data) > 0
-    print(data)
+@allure.feature('Pet Store API')
+class TestPetStore:
+    config = Config()
+
+    @allure.story('Get Pet by ID')
+    def test_get_pet_by_id(self):
+        url, response = self.get_pet(self.config.pet_id)
+        assert response.json()['id'] == self.config.pet_id, "Pet ID does not match"
+
+    @allure.story('Get Pet Response is not Empty')
+    def test_get_pet_response_is_not_empty(self):
+        url, response = self.get_pet(self.config.pet_id)
+        assert len(response.json()) > 0, "Response is empty"
+
+    @allure.story('Add New Pet')
+    def test_add_new_pet(self):
+        url, response = self.add_pet(self.config.new_pet_id, self.config.new_pet_name, self.config.new_pet_status)
+        assert response.json()['id'] == self.config.new_pet_id, "New Pet ID does not match"
+        assert len(response.json()) > 0, "Response is empty"
+
+    def get_pet(self, pet_id: int) -> Tuple[str, requests.Response]:
+        url = urljoin(self.config.base_url, str(pet_id))
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(url, verify=False, headers=headers)
+        return url, response
+
+    def add_pet(self, pet_id: int, pet_name: str, pet_status: str) -> Tuple[str, requests.Response]:
+        url = self.config.base_url
+        headers = {'Content-Type': 'application/json'}
+        payload: Dict[str, str] = {'id': str(pet_id), 'name': pet_name, 'status': pet_status}
+        response = requests.post(url, verify=False, json=payload, headers=headers)
+        return url, response
